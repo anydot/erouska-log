194a246f87ed3bcffcd5556900158eb8a94ee7fd
Premysl Hruby <dfenze@gmail.com>
2021-03-15 18:40:42 +0000

    Save also the UZIS datasets

--- scrape.sh
+++ scrape.sh
@@ -3,8 +3,35 @@ set -eu
 
 baseurl="https://storage.googleapis.com/exposure-notification-export-qhqcx"
 datadir="erouska"
+uzisdir="uzis"
 
-mkdir -p $datadir
+uzisSources=(
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/testy-pcr-antigenni.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/testy.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/kraj-okres-testy.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakaza.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakazeni-vyleceni-umrti-testy.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/orp.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/obce.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/mestske-casti.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/hospitalizace.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovaci-mista.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/prehled-ockovacich-mist.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/prehled-odberovych-mist.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-spotreba.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-distribuce.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-registrace.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-rezervace.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/osoby.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/kraj-okres-nakazeni-vyleceni-umrti.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/vyleceni.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/umrti.json
+    https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/pomucky.json
+    )
+
+mkdir -p $datadir $uzisdir
 
 curl "$baseurl/erouska/index.txt" | while read file; do
     outfile=$datadir/`basename $file .zip`.json
@@ -16,3 +43,11 @@ curl "$baseurl/erouska/index.txt" | while read file; do
         rm -f $tmp
     fi
 done
+
+## UZIS data
+
+for uzisDataset in ${uzisSources[@]}; do
+    curl $uzisDataset -o $uzisdir/`basename $uzisDataset`
+done
+
+
